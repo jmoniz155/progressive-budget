a7c453b1d23b9228dde0a3c79b370e75db4cb355
Jesse Moniz <jmoniz155@icloud.com>
2021-11-17 15:17:43 -0800

    fix transaction.js

--- models/transaction.js
+++ models/transaction.js
@@ -1,22 +1,22 @@
 const mongoose = require("mongoose");
 
-const Schema = mongoose.Schema;
-
+const { Schema } = mongoose;
+// create database transaction
 const transactionSchema = new Schema(
   {
     name: {
       type: String,
       trim: true,
-      required: "Enter a name for transaction"
+      required: "Enter a name for transaction",
     },
     value: {
       type: Number,
-      required: "Enter an amount"
+      required: "Enter an amount",
     },
     date: {
       type: Date,
-      default: Date.now
-    }
+      default: Date.now,
+    },
   }
 );
 
