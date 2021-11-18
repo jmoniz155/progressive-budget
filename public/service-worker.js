bb2def3014bda2015fe6f04b3754f2a53c7c0e59
Jesse Moniz <jmoniz155@icloud.com>
2021-11-18 15:57:49 -0800

    fix service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,21 +4,81 @@ const FILES_TO_CACHE = [
     "/",
     "/index.html",
     "/manifest.webmanifest",
-    "icons/icon-192x192.png",
-    "icons/icon-512x512",
+    "/db.js",
+    "/styles.css",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png",
 ];
 
 const CACHE_NAME = "static-cache-v1";
 const DATA_CACHE_NAME = "data-cache-v1";
 
-// install
-self.addEventListener("install", function(evt) {
+// install, triggered immediately after serrvice worker is registered
+// evt.waitUntil letting the browser know when installation is finished with service worker
+self.addEventListener("install", function (evt) {
     evt.waitUntil(
-      caches.open(CACHE_NAME).then(cache => {
-        console.log("Your files were pre-cached successfully!");
-        return cache.addAll(FILES_TO_CACHE);
+        caches.open(CACHE_NAME).then(cache => {
+            console.log("Your files were pre-cached successfully!");
+            return cache.addAll(FILES_TO_CACHE);
+        })
+    );
+
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", function(evt) {
+    evt.waitUntil(
+      caches.keys().then(keyList => {
+        return Promise.all(
+          keyList.map(key => {
+            if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+              console.log("Removing old cache data", key);
+              return caches.delete(key);
+            }
+          })
+        );
       })
     );
   
-    self.skipWaiting();
+    self.clients.claim();
+});
+
+self.addEventListener("fetch", function(evt) {
+    // Will will be using TWO different approaches to handling requests.
+    // Therefore, a conditional is required to determine how to handle a request.
+  
+    // #1: Handle requests for frequently changing data by checking if the route
+    // contains "/api".
+    if (evt.request.url.includes("/api/transaction")) {
+      // Implement Network falling back to the cache pattern. (See
+      evt.respondWith(
+        caches.open(DATA_CACHE_NAME).then(cache => {
+          return fetch(evt.request)
+            .then(response => {
+              // If the response was good, clone it and store it in the cache.
+              if (response.status === 200) {
+                cache.put(evt.request.url, response.clone());
+              }
+  
+              return response;
+            })
+            .catch(err => {
+              // Network request failed, try to get it from the cache.
+              return cache.match(evt.request);
+            });
+        }).catch(err => console.log(err))
+      );
+  
+      return;
+    }
+  
+    // #2: Handle requests for static items. (html, css, js, etc)
+    // Implement "offline-first" pattern. (aka "cache falling back to network";
+    // see
+    // https://developers.google.com/web/fundamentals/instant-and-offline/offline-cookbook#cache-falling-back-to-network)
+    evt.respondWith(
+      caches.match(evt.request).then(function(response) {
+        return response || fetch(evt.request);
+      })
+    );
   });
