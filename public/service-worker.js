34496ef5ec9ac97074b6f6d8ba7fe500d7c8fca7
Jesse Moniz <jmoniz155@icloud.com>
2021-11-18 09:47:28 -0800

    adding to cache  service worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,1 +1,24 @@
 console.log("Hello from service worker!")
+
+const FILES_TO_CACHE = [
+    "/",
+    "/index.html",
+    "/manifest.webmanifest",
+    "icons/icon-192x192.png",
+    "icons/icon-512x512",
+];
+
+const CACHE_NAME = "static-cache-v1";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+// install
+self.addEventListener("install", function(evt) {
+    evt.waitUntil(
+      caches.open(CACHE_NAME).then(cache => {
+        console.log("Your files were pre-cached successfully!");
+        return cache.addAll(FILES_TO_CACHE);
+      })
+    );
+  
+    self.skipWaiting();
+  });
